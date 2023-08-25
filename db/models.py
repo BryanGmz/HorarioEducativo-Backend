d7730af3a79fc3148c061c0a6282a3f388c4b5bb
BryanGmz <brgg13@gmail.com>
2023-08-24 18:45:50 -0600

    Add: Database models are added.

---
+++ db/models.py
@@ -0,0 +1,90 @@
+from db.database import Base
+from sqlalchemy import Column, Integer, String, Time, ForeignKey, BigInteger, PrimaryKeyConstraint
+from sqlalchemy.orm import relationship
+
+class Carrer(Base):
+    __tablename__ = "carrer"
+    id_carrer = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    courses = relationship("Course", back_populates="courses")
+    assignments = relationship("Assignment", back_populates="assignments")
+
+class Course(Base): 
+    __tablename__ = "course"
+    id_course = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    semester = Column(Integer, nullable=False)
+    carrer_id = Column(Integer, ForeignKey("carrer.id_carrer"))
+    carrers = relationship("Carrer", back_populates="carrers")
+    assignments = relationship("Assignment", back_populates="assignments")
+
+class Assignment(Base):
+    __tablename__ = "assignment"
+    course_id = Column(Integer, ForeignKey("course.id_course"), primary_key=True)
+    carrer_id = Column(Integer, ForeignKey("carrer.id_carrer"), primary_key=True)
+    year = Column(Integer, primary_key=True)
+    semester = Column(Integer, primary_key=True)
+    assigned = Column(Integer, nullable=False)
+    courses = relationship("Course", back_populates="courses")
+    carrers = relationship("Carrer", back_populates="carrers")
+
+    __table_args__ = (
+        PrimaryKeyConstraint("course_id", "carrer_id", "year", "semester"),
+    )
+
+class Area(Base):
+    __tablename__ = "area"
+    id_area = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    requirements = relationship("Requeriment", back_populates="requeriments")
+
+class Teacher(Base):
+    __tablename__ = "teacher"
+    dpi_teacher = Column(BigInteger, primary_key=True)
+    name = Column(String, nullable=False)
+    specialties = relationship("Specialty", back_populates="specialties")
+
+class Classroom(Base):
+    __tablename__ = "classroom"
+    id_classroom = Column(Integer, primary_key=True)
+    capacity = Column(Integer, nullable=False)
+    name = Column(String, nullable=False)
+
+class Requirement(Base):
+    __tablename__ = "requeriment"
+    area_id = Column(Integer, ForeignKey("area.id_area"), primary_key=True)
+    course_id = Column(Integer, ForeignKey("course.id_course"), primary_key=True)
+    areas = relationship("Area", back_populates="areas")
+    courses = relationship("Course", back_populates="courses")
+
+    __table_args__ = (
+        PrimaryKeyConstraint("area_id", "course_id"),
+    )
+
+class Specialty(Base):
+    __tablename__ = "specialty"
+    area_id = Column(Integer, ForeignKey("area.id_area"), primary_key=True)
+    teacher_dpi = Column(BigInteger, ForeignKey("teacher.dpi_teacher"), primary_key=True)
+    areas = relationship("Area", back_populates="areas")
+    teachers = relationship("Teacher", back_populates="teachers")
+
+    __table_args__ = (
+        PrimaryKeyConstraint("area_id", "teacher_dpi"),
+    )
+
+class ScheduleAssignment(Base):
+    __tablename__ = "schedule_assignment"
+    course_id = Column(Integer, ForeignKey("course.id_course"), primary_key=True)
+    classroom_id = Column(Integer, ForeignKey("classroom.id_classroom"), primary_key=True)
+    teacher_dpi = Column(BigInteger, ForeignKey("teacher.dpi_teacher"), primary_key=True)
+    year = Column(Integer, nullable=False)
+    semester = Column(Integer, nullable=False)
+    star_time = Column(Time, nullable=False)
+    end_time = Column(Time, nullable=False)
+    courses = relationship("Course", back_populates="courses")
+    classrooms = relationship("Classroom", back_populates="classrooms")
+    teachers = relationship("Teacher", back_populates="teachers")
+
+    __table_args__ = (
+        PrimaryKeyConstraint("course_id", "classroom_id", "teacher_dpi"),
+    )
---
+++ main.py
@@ -0,0 +1,26 @@
+from typing import Union
+from db.database import Base, engine, get_db
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from db.models import Course
+import uvicorn
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+create_tables()
+
+app = FastAPI()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
+
+@app.get("/")
+def read_root(db:Session = Depends(get_db)):
+    data = db.query(Course).all()
+    print(data)
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
